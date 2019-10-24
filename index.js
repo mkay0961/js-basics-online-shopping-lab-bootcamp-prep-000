b15080ed5c0cbaafb65d0788e708210956eb48f9
Matthew Kay <mkay0961@gmail.com>
2019-10-24 13:02:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ for (let i = 0; i<cart.length;i++){
   console.log(cart);
   return cart
 } else  if (item != cart[i]["itemName"]) {
-console.log('That item is not in your cart.');
+console.log("'That item is not in your cart.'");
 return 'That item is not in your cart.'
 }
 }
