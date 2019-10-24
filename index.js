e7484799a091685185c80b535b8621c308527c51
Matthew Kay <mkay0961@gmail.com>
2019-10-24 22:46:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -50,17 +50,6 @@ let total = 0
   return total
 }
 
-function placeOrder(cardNumber) {
-  if (!cardNumber){
-  return "Sorry, we don't have a credit card on file for you."
-} else {
-  cart = []
-console.log(`${total()} this is the test total`)
-  return (`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
-
-}
-}
-
 function removeFromCart(item) {
 
 for (let i = 0; i<cart.length;i++){
@@ -72,4 +61,15 @@ for (let i = 0; i<cart.length;i++){
 }
 }
 return 'That item is not in your cart.'
+}
+
+function placeOrder(cardNumber) {
+  if (!cardNumber){
+  return "Sorry, we don't have a credit card on file for you."
+} else {
+  
+console.log(`${total()} this is the test total`)
+  return (`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
+
+}
 }
